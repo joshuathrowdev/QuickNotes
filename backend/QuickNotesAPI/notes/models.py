961e530294ec8a5e52e9c034eb2e4506899cfdcf
Joshua <joshuathrow.dev@gmail.com>
2025-05-24 15:27:27 -0500

    feat: created and handle the PATCH, DELETE, and POST methods for the endpoints and rounded out the database

--- backend/QuickNotesAPI/notes/models.py
+++ backend/QuickNotesAPI/notes/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Note(models.Model):
-  title = models.CharField(verbose_name="Title", max_length=150, null=False, blank=False)
+  title = models.CharField(verbose_name="Title", null=False, blank=False)
   body = models.TextField(verbose_name="Body", null=False, blank=False)
+  created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
   def __str__(self):
     return self.title
--- backend/QuickNotesAPI/notes/views.py
+++ backend/QuickNotesAPI/notes/views.py
@@ -21,28 +21,60 @@ class ListNotesView(APIView):
   # We have to pass the self pointer and the request
 
   def get(self, request):
-    notes = Note.objects.get()
-    serializer = NoteSerializer(notes) # pass the serializer the instance (object(s)) that you want it to serializer
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True) # pass the serializer the instance (object(s)) that you want it to serializer
     # Remember it will auto map the data since we define the innter meta class
+    # If we are getting a container/wrapper that contains many objects, we have to set the 
+    # argument value of many to True to let the serializer know that there are many objects
 
     return Response(serializer.data, status=status.HTTP_200_OK)
-  # The Response function auto wraps any datatype in the correct pythonic container
+    # The Response function auto wraps any datatype in the correct pythonic container
+
+  def post(self, request):
+    serializer = NoteSerializer(data=request.data) # Deserialization of Incoming request data
+
+    if serializer.is_valid(): # Validation of request
+      serializer.save() # saving instance to database
+      return Response(serializer.data, status=status.HTTP_201_CREATED) # returning newly created note in reponse 
+  
+    # Non valid request data
+    return Response(status=status.HTTP_400_BAD_REQUEST)  
 
 
 # Function Based View 
-@api_view(['GET']) # Defining what HTTP methods it has access to
+@api_view(['GET', 'PATCH', 'DELETE']) # Defining what HTTP methods it has access to
+# In the REST Framework, the endpoint is responsible for a resource: in this case its the note
+# It does not violate SRP if this singular endpoint is allowed to do multiple HTTP methods
 def NoteDetailsView(request, note_id):
 
+  # Querying for specific record and handleing does not exist error
+  try:
+    note = Note.objects.get(pk=note_id)
+  except Note.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+
   if request.method == 'GET':
-    try:
-      note = Note.objects.get(pk=note_id)
-      serializer = NoteSerializer(note)
+    serializer = NoteSerializer(note)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+  if request.method == 'PATCH':
+    serializer = NoteSerializer(note, data=request.data, partial=True)
+    # To updated (PUT or PATCH a record) we have to pass the serializer the the specific
+    # record first, and then the updating data (from the request)
+    # The serializer looks at the updated fields from the request and compares it to the instance
+    # In a PATCH request, the serializer has to also be set to partial=True so it knows that
+    # we can pass partial (PATCH updated) (validation redundency)
+    if serializer.is_valid(): # checking validation after data update
+      serializer.save()
 
       return Response(serializer.data, status=status.HTTP_200_OK)
-    except:
-      return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+  
+  if request.method == "DELETE":
+    note.delete()
+    return Response(status=status.HTTP_200_OK)
 
-
+  return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 # How Views and Serializers work together
